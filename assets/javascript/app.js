35f5a17bb8a877d7e16f964a55ea1d2736ca345c
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-02-27 14:22:46 -0800

    printing the articles that are found in the search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,25 +12,40 @@ $(document).ready(function () {
       url:Url,
       method:"GET"
     }).then(function (data) {
-      console.log(data);
+      console.log(data.response.docs);
+      for (var i = 0; i < data.response.docs.length; i++) {
+        console.log("here");
+        var article = $("<div>");
+        var form = $("<form target='_blank'>");
+        var artButton = $("<button>").text(data.response.docs[i].headline.print_headline);
+        form.attr("action", data.response.docs[i].web_url);
+        form.append(artButton);
+        article.append(form);
+        $("#article").append(article);
+      }
     });
   }
 
   $("#search").on("click",function (event) {
     event.preventDefault();
     var searchTerm = $("#input1").val().trim();
-    term += searchTerm;
-    queryUrl += term;
-    count = $("#input2").val().trim();
     var beginDate = $("#input3").val().trim();
-    begin += beginDate;
-    queryUrl += begin;
     var endDate = $("#input4").val().trim();
-    end += endDate;
-    queryUrl += end;
+    count = $("#input2").val().trim();
+    if(searchTerm !== ""){
+      term += searchTerm;
+      queryUrl += term;
+    }
+    if(beginDate !== ""){
+      begin += beginDate;
+      queryUrl += begin;
+    }
+    if(endDate !== ""){
+      end += endDate;
+      queryUrl += end;
+    }
     console.log(queryUrl);
     callAjax(queryUrl);
   });
-
 
 });
